5b8745195f64e5b6be4273559978f50be2c71bf5
tzecris <tzecris13@gmail.com>
2020-05-11 18:37:35 +0300

    service problem fix

--- src/main/java/com/app/pharmacy/model/Ingredient.java
+++ src/main/java/com/app/pharmacy/model/Ingredient.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.app.pharmacy.model;
 
 import java.io.Serializable;
@@ -14,27 +9,24 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
  * @author Admin
  */
 @Entity
-@Table(catalog = "pharmacy_app", schema = "")
+@Table(name = "ingedient")
 @XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "Ingredient.findAll", query = "SELECT i FROM Ingredient i")
-    , @NamedQuery(name = "Ingredient.findByIngredientId", query = "SELECT i FROM Ingredient i WHERE i.ingredientId = :ingredientId")
-    , @NamedQuery(name = "Ingredient.findByName", query = "SELECT i FROM Ingredient i WHERE i.name = :name")
-    , @NamedQuery(name = "Ingredient.findByStock", query = "SELECT i FROM Ingredient i WHERE i.stock = :stock")
-    , @NamedQuery(name = "Ingredient.findByPrice", query = "SELECT i FROM Ingredient i WHERE i.price = :price")})
+//@NamedQueries({
+//    @NamedQuery(name = "Ingredient.findAll", query = "SELECT i FROM Ingredient i")
+//    , @NamedQuery(name = "Ingredient.findByIngredientId", query = "SELECT i FROM Ingredient i WHERE i.ingredientId = :ingredientId")
+//    , @NamedQuery(name = "Ingredient.findByName", query = "SELECT i FROM Ingredient i WHERE i.name = :name")
+//    , @NamedQuery(name = "Ingredient.findByStock", query = "SELECT i FROM Ingredient i WHERE i.stock = :stock")
+//    , @NamedQuery(name = "Ingredient.findByPrice", query = "SELECT i FROM Ingredient i WHERE i.price = :price")})
 public class Ingredient implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -88,39 +80,4 @@ public class Ingredient implements Serializable {
     public void setPrice(Long price) {
         this.price = price;
     }
-
-    @XmlTransient
-    public Collection<IngredientOrderVip> getIngredientOrderVipCollection() {
-        return ingredientOrderVipCollection;
-    }
-
-    public void setIngredientOrderVipCollection(Collection<IngredientOrderVip> ingredientOrderVipCollection) {
-        this.ingredientOrderVipCollection = ingredientOrderVipCollection;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (ingredientId != null ? ingredientId.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Ingredient)) {
-            return false;
-        }
-        Ingredient other = (Ingredient) object;
-        if ((this.ingredientId == null && other.ingredientId != null) || (this.ingredientId != null && !this.ingredientId.equals(other.ingredientId))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "com.app.pharmacy.model.Ingredient[ ingredientId=" + ingredientId + " ]";
-    }
-
 }
--- src/main/java/com/app/pharmacy/service/OrderVipServiceImpl.java
+++ src/main/java/com/app/pharmacy/service/OrderVipServiceImpl.java
@@ -8,9 +8,10 @@ import com.app.pharmacy.repository.OrderVipRepository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
+@Service
 public class OrderVipServiceImpl implements OrderVipService{
     
     @Autowired
