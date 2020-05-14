f0d68720bccf7dfcc89ae4bc8746ca2682cf75e5
tzecris <tzecris13@gmail.com>
2020-05-14 21:43:46 +0300

    categories enum

---
+++ src/main/java/com/app/pharmacy/model/CategoryEnum.java
@@ -0,0 +1,9 @@
+package com.app.pharmacy.model;
+
+/**
+ *
+ * @author Chris Tzelis
+ */
+public enum CategoryEnum {
+    PILLS, WOMEN, MAN, CHILD, PAINKILLER, ANTIBIOTIC, DENTAL, SEXUAL_HEALTH, VITAMIN, ANTIDEPRESSANT, GEL,
+}
