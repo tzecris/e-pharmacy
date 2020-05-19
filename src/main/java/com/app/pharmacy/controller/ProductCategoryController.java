b8c6d9c1a72e92ba95c2c0ed50f789010adf5224
chrismagiriass <64489026+chrismagiriass@users.noreply.github.com>
2020-05-19 19:26:43 +0300

    new product_category

--- src/main/java/com/app/pharmacy/controller/ProductCategoryController.java
+++ src/main/java/com/app/pharmacy/controller/ProductCategoryController.java
@@ -5,10 +5,53 @@
  */
 package com.app.pharmacy.controller;
 
-/**
- *
- * @author Christos
- */
+import com.app.pharmacy.dto.ProductCategoryDTO;
+import com.app.pharmacy.dto.ResponseMessage;
+import com.app.pharmacy.service.ProductCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("api/productcategories")
 public class ProductCategoryController {
     
+    @Autowired
+    ProductCategoryService productCategoryService;
+
+    @GetMapping
+    public ResponseEntity findAll() {
+
+        return ResponseEntity.ok(productCategoryService.findAll());
+    }
+    
+    @PostMapping
+    public ResponseEntity createProductCategory(@RequestBody ProductCategoryDTO p) {
+        productCategoryService.save(p);
+        return ResponseEntity.ok(new ResponseMessage("Category created succesfully!"));
+  }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity getProductCategory(@PathVariable Integer id) {
+        return ResponseEntity.ok(productCategoryService.findById(id));
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProductCategory(@PathVariable Integer id) {
+        productCategoryService.deleteById(id);
+        return ResponseEntity.ok(new ResponseMessage("Category deleted successfully!"));
+    }
+    
+    
+
 }
+
+
+    
