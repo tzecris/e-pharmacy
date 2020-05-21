5ade8cfde1452c76c3979a19cec76b1e5fc3a01d
stathis-xideros <59621501+stathis-xideros@users.noreply.github.com>
2020-05-21 22:34:54 +0200

    websocket

---
+++ src/main/java/com/app/pharmacy/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.app.pharmacy.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+@ComponentScan(basePackages = "be.g00glen00b", excludeFilters = {
+    @ComponentScan.Filter(value = Controller.class, type = FilterType.ANNOTATION),
+    @ComponentScan.Filter(value = Configuration.class, type = FilterType.ANNOTATION)
+})
+public class AppConfig {
+    
+}
